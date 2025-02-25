d18eea485c0af62cccda2c72c6d6310d1849f7c0
Prinse Q12 <132322504+Prinse112@users.noreply.github.com>
2025-02-25 18:40:35 +0500

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'SUPUN-MD=SA0XFbAC#mJSZSBzfnxxxwZ5GYyerqjug4eCE4PW5DLvdN9FkAjk', //add your session id
+SESSION_ID: process.env.SESSION_ID || 'SUPUN-MD=zN1mxZLC#txa4pOyjq5bIjVP0BDARv_UDfVq_wSBuPoAmfJkXTYY', //add your session id
 MONGODB: process.env.MONGODB || "mongodb://mongo:dHulUqJfRHKpFiGOfKqBNobiOpwaZzct@crossover.proxy.rlwy.net:16412", //add your mongodb url
  URL : process.env.BOT_MENU_LINKS || 'https://i.ibb.co/bHXBV08/9242c844b83f7bf9.jpg',
 MAX_SIZE: 500
